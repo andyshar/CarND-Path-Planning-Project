78efe6a281f35749ba12db0bf9e7677bd390ad1e
AndyShar <andyshar1216@msn.com>
2019-02-07 23:54:13 +0800

    Define lane and ref_vel

--- src/main.cpp
+++ src/main.cpp
@@ -52,8 +52,11 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+  int lane = 1;
+  double ref_vel = 0.0;
+
+  h.onMessage([&ref_vel, &lane, &map_waypoints_x, &map_waypoints_y, &map_waypoints_s, 
+               &map_waypoints_dx, &map_waypoints_dy]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
